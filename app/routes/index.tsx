f844080e54650e098c0f1f2aeac930c62b51e666
wintersieck <awintersieck@gmail.com>
2022-06-28 15:57:16 -0600

    minimal reproduction

---
+++ app/routes/index.tsx
@@ -0,0 +1,64 @@
+import algoliasearch from "algoliasearch/lite";
+import { useState } from "react";
+import {
+  Hits,
+  InstantSearch,
+  RefinementList,
+} from "react-instantsearch-hooks-web";
+
+type AlgoliaWrapperProps = {
+  propToForceRerender: string;
+};
+
+function AlgoliaWrapper({ propToForceRerender }: AlgoliaWrapperProps) {
+  const appId = "RHVCHJW67L";
+  const indexName = "demo_content";
+  const apiKey = "5e56001b6c4b76edb3d2b69ca5951b75";
+  const [searchClient] = useState(algoliasearch(appId, apiKey));
+  const initialUiState = {
+    [indexName]: {
+      refinementList: {
+        "metadata.tags.sys.id": ["blog"],
+      },
+    },
+  };
+
+  console.log("AlgoliaWrapper rendered", propToForceRerender);
+
+  return (
+    <InstantSearch
+      indexName={indexName}
+      searchClient={searchClient}
+      initialUiState={initialUiState}
+    >
+      <div>
+        <RefinementList
+          attribute="metadata.tags.sys.id"
+          // Using example code from https://www.algolia.com/doc/api-reference/widgets/refinement-list/react-hooks/#widget-param-transformitems
+          transformItems={(items) => {
+            return items.map((item) => ({
+              ...item,
+              label: item.label.toUpperCase(),
+            }));
+          }}
+        />
+      </div>
+      <div>
+        <Hits />
+      </div>
+    </InstantSearch>
+  );
+}
+
+export default function Index() {
+  const [value, setValue] = useState("test1");
+  setTimeout(() => {
+    console.log("Updating a test prop to force AlgoliaWrapper to re-render");
+    setValue("test2");
+  }, 2000);
+  return (
+    <div>
+      <AlgoliaWrapper propToForceRerender={value} />
+    </div>
+  );
+}
